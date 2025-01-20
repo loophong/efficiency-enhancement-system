c910a684ce9d8528827dcabe629945fe62e015b6
loophong <1940155085@qq.com>
2025-01-20 17:34:14 +0800

    refactor(production): 重构产能相关代码
    - 将 CapacityEntity 中的 vehicleModel 字段改为 capacityType
    - 更新 CapacityMapper.xml 中的相关 SQL 语句
    - 修改前端页面中与车型相关的属性和方法
    - 优化加急订单处理逻辑，增加排产检查和统计已使用产能的功能

--- ruoyi-ui/src/views/production/scheduling/index.vue
+++ ruoyi-ui/src/views/production/scheduling/index.vue
@@ -38,7 +38,7 @@
         </el-form-item>
 
         <div v-for="(item, index) in capacity" :key="index">
-          <el-form-item :label="item.vehicleModel">
+          <el-form-item :label="item.capacityType">
             <el-input-number v-model="item.productionQuantity" :min="0"></el-input-number>
           </el-form-item>
         </div>
@@ -46,19 +46,19 @@
 
     </div>
 
-    <div v-show="active === 2" >
+    <div v-show="active === 2">
       <div style="display: flex; margin: 20px; width:95%;">
         <div style="flex: 1; border: #00afff solid;">
           <!--   渲染产能总数和已使用产能数量   -->
           <el-form label-width="120px">
             <el-col style="margin: 20px">
               <div v-for="(item, index) in usedCapacity" :key="index">
-                <el-form-item :label="item.vehicleModel">
+                <el-form-item :label="item.capacityType">
                   <el-input v-model="item.productionQuantity" readonly style="width: 150px;"
                             input-style="font-size: large; font-weight: bold;" size="large" placeholder="Please input">
                     <template #append>
                       <span style="width: 30px; font-size: large; font-weight: bold;">
-                       {{ getCapacity(item.vehicleModel) }}
+                       {{ getCapacity(item.capacityType) }}
                       </span>
                     </template>
                   </el-input>
@@ -70,7 +70,8 @@
 
         <div style="flex: 4; border: #2d2d2d solid;">
           <!--   展示加急订单   -->
-          <el-table v-loading="loading" :data="urgentList" width="auto" @selection-change="handleUrgentSelectionChange">
+          <el-table ref="urgentTable" v-loading="loading" :data="urgentList" width="auto"
+                    @selection-change="handleUrgentSelectionChange">
             <el-table-column type="selection" width="55" align="center"/>
             <el-table-column label="网点名称" align="center" prop="branch"/>
             <el-table-column label="合同号" align="center" prop="contractNumber"/>
@@ -82,18 +83,18 @@
             </el-table-column>
             <el-table-column label="车型" align="center" prop="vehicleModel" width="100"/>
             <el-table-column label="数量" align="center" prop="quantity"/>
-<!--            <el-table-column label="车号" align="center" prop="vehicleNumber"/>-->
+            <!--            <el-table-column label="车号" align="center" prop="vehicleNumber"/>-->
             <el-table-column label="备注信息" align="center" prop="remarks" width="150"/>
             <el-table-column label="订单系统交货期" align="center" prop="systemDeliveryDate" width="100">
               <template #default="scope">
                 <span>{{ parseTime(scope.row.systemDeliveryDate, '{y}-{m}-{d}') }}</span>
               </template>
             </el-table-column>
-<!--            <el-table-column label="生产回复完工日期" align="center" prop="productionCompletionDate" width="100">-->
-<!--              <template #default="scope">-->
-<!--                <span>{{ parseTime(scope.row.productionCompletionDate, '{y}-{m}-{d}') }}</span>-->
-<!--              </template>-->
-<!--            </el-table-column>-->
+            <!--            <el-table-column label="生产回复完工日期" align="center" prop="productionCompletionDate" width="100">-->
+            <!--              <template #default="scope">-->
+            <!--                <span>{{ parseTime(scope.row.productionCompletionDate, '{y}-{m}-{d}') }}</span>-->
+            <!--              </template>-->
+            <!--            </el-table-column>-->
             <el-table-column label="采购回复到货时间" align="center" prop="procurementArrivalDate" width="100">
               <template #default="scope">
                 <span>{{ parseTime(scope.row.procurementArrivalDate, '{y}-{m}-{d}') }}</span>
@@ -107,28 +108,28 @@
               </template>
             </el-table-column>
             <el-table-column label="是否超期" align="center" prop="isOverdue"/>
-<!--            <el-table-column label="是否加急" align="center" prop="isUrgent">-->
-<!--              <template #default="scope">-->
-<!--                <el-tag v-if="scope.row.isUrgent === 0" type="info">否</el-tag>-->
-<!--                <el-tag v-else-if="scope.row.isUrgent === 1" type="danger">是</el-tag>-->
-<!--              </template>-->
-<!--            </el-table-column>-->
-<!--            <el-table-column label="是否排产" align="center" prop="isScheduling">-->
-<!--              <template #default="scope">-->
-<!--                <el-tag v-if="scope.row.isScheduling === 0" type="info">未排产</el-tag>-->
-<!--                <el-tag v-else-if="scope.row.isScheduling === 1" type="success">已排产</el-tag>-->
-<!--              </template>-->
-<!--            </el-table-column>-->
-<!--            <el-table-column label="操作" align="center" class-name="small-padding fixed-width">-->
-<!--              <template #default="scope">-->
-<!--                <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"-->
-<!--                           v-hasPermi="['production:scheduling:edit']">修改-->
-<!--                </el-button>-->
-<!--                <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"-->
-<!--                           v-hasPermi="['production:scheduling:remove']">删除-->
-<!--                </el-button>-->
-<!--              </template>-->
-<!--            </el-table-column>-->
+            <!--            <el-table-column label="是否加急" align="center" prop="isUrgent">-->
+            <!--              <template #default="scope">-->
+            <!--                <el-tag v-if="scope.row.isUrgent === 0" type="info">否</el-tag>-->
+            <!--                <el-tag v-else-if="scope.row.isUrgent === 1" type="danger">是</el-tag>-->
+            <!--              </template>-->
+            <!--            </el-table-column>-->
+            <!--            <el-table-column label="是否排产" align="center" prop="isScheduling">-->
+            <!--              <template #default="scope">-->
+            <!--                <el-tag v-if="scope.row.isScheduling === 0" type="info">未排产</el-tag>-->
+            <!--                <el-tag v-else-if="scope.row.isScheduling === 1" type="success">已排产</el-tag>-->
+            <!--              </template>-->
+            <!--            </el-table-column>-->
+            <!--            <el-table-column label="操作" align="center" class-name="small-padding fixed-width">-->
+            <!--              <template #default="scope">-->
+            <!--                <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"-->
+            <!--                           v-hasPermi="['production:scheduling:edit']">修改-->
+            <!--                </el-button>-->
+            <!--                <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"-->
+            <!--                           v-hasPermi="['production:scheduling:remove']">删除-->
+            <!--                </el-button>-->
+            <!--              </template>-->
+            <!--            </el-table-column>-->
           </el-table>
         </div>
       </div>
@@ -156,7 +157,9 @@
 import {ref, reactive} from "vue";
 import {all} from "@/api/production/capacity.js";
 import {getUrgentOrder} from "@/api/production/orderScheduling.js";
+import {getCurrentInstance} from 'vue';
 
+const {proxy} = getCurrentInstance();
 
 // 初始化步骤条为1
 let active = ref(1);
@@ -191,11 +194,11 @@ function getCapacityList() {
     // 从response.data中解析出车型 和 产量
     response.data.forEach(item => {
       capacity.value.push({
-        'vehicleModel': item.vehicleModel,
+        'capacityType': item.capacityType,
         'productionQuantity': item.productionQuantity
       })
       usedCapacity.value.push({
-        'vehicleModel': item.vehicleModel,
+        'capacityType': item.capacityType,
         'productionQuantity': 0
       })
     })
@@ -209,8 +212,8 @@ onMounted(() => {
   getUrgentOrderList();
 });
 
-function getCapacity(vehicleModel) {
-  const used = capacity.value.find(item => item.vehicleModel === vehicleModel);
+function getCapacity(capacityType) {
+  const used = capacity.value.find(item => item.capacityType === capacityType);
   return used ? used.productionQuantity : 0;
 }
 
@@ -222,12 +225,76 @@ function getUrgentOrderList() {
   })
 }
 
-// 多选框选中数据
+const schedulingUrgentList = ref([]);
+
+// 多选框选中加急排产数据
 function handleUrgentSelectionChange(selection) {
-  console.log("多选框选中数据：" + JSON.stringify(selection))
-  // ids.value = selection.map(item => item.id);
-  // single.value = selection.length != 1;
-  // multiple.value = !selection.length;
+  console.log('selection', selection);
+  // 检查当前型号是否进行排产
+  selection.forEach((item, index) => {
+    if (!checkCapacityType(item.capacityType)) {
+      // 询问用户是否对当前车型进行排产
+      proxy.$confirm('当前车型未进行排产，是否对当前车型进行排产？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        // 跳转到排产页面
+        capacity.value.push({
+          'capacityType': item.capacityType,
+          'productionQuantity': 0
+        })
+        usedCapacity.value.push({
+          'capacityType': item.capacityType,
+          'productionQuantity': 0
+        })
+        schedulingUrgentList.value = selection;
+        countUsedCapacity()
+      }).catch(() => {
+        // 取消操作,将当前选中的数据取消选中
+        console.log('selection-before', selection);
+        selection.splice(index, 1);
+        proxy.$refs.urgentTable.toggleRowSelection(item, false);
+        console.log('selection-after', selection);
+
+      });
+    }
+  })
+  console.log("下一步操作：统计")
+  schedulingUrgentList.value = selection;
+  countUsedCapacity()
+}
+
+// 检查当前型号是否在capacity中
+function checkCapacityType(capacityType) {
+  return capacity.value.find(item => item.capacityType === capacityType);
+}
+
+// 统计已使用产能
+function countUsedCapacity() {
+  console.log("开始统计已使用产能")
+
+  usedCapacity.value.forEach(item => {
+    item.productionQuantity = 0;
+  })
+  usedCapacity.value.forEach(item => {
+    // 从加急订单中统计已使用产能
+    let urgentNum = getUsedCapacity(item.capacityType, schedulingUrgentList);
+    // 从临期订单中统计已使用产能
+    // let urgentNum = getUsedCapacity(item.capacityType, schedulingUrgentList);
+    // 从正常订单中统计已使用产能
+    // let urgentNum = getUsedCapacity(item.capacityType, schedulingUrgentList);
+    item.productionQuantity = urgentNum;
+  })
+  console.log("已使用产能：" + JSON.stringify(usedCapacity.value))
+}
+
+// 根据车型获取已使用产能
+function getUsedCapacity(capacityType, list) {
+  console.log("根据车型获取已使用产能：" + capacityType)
+  console.log("车型list：" + JSON.stringify(list))
+  const used = list.value.find(item => item.capacityType === capacityType);
+  return used ? used.quantity : 0;
 }
 
 </script>
